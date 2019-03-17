f99b9746c95b5b2abb764c1ec900b1946632612c
loktevsp <sufisk0@spaces.ru>
2019-03-17 16:28:38 +0300

    Added bootstrap

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,8 +1,25 @@
+<?php $this->beginPage(); ?>
 <html>
   <head>
     <title>Avto.ru</title>
+    <?php $this->head(); ?>
   </head>
   <body>
-    <?= $content ?>
+    <?php $this->beginBody(); ?>
+    <?php
+      \yii\bootstrap\NavBar::begin([
+        'brandLabel' => '<b>Avto.ru</b>',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+          'class' => 'navbar-default navbar-fixed-top'
+          ]
+      ]);
+      \yii\bootstrap\NavBar::end();
+    ?>
+    <div class="container" style="margin-top: 50px">
+      <?= $content ?>
+    </div>
+    <?php $this->endBody(); ?>
   </body>
 </html>
+<?php $this->endPage(); ?>
