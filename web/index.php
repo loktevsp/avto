63d32a48048042e6c8b1b234bf8811fe728eda8a
loktevsp <sufisk0@spaces.ru>
2019-03-17 12:00:34 +0300

    Add .gitignore

--- web/index.php
+++ web/index.php
@@ -1,1 +1,6 @@
-Hello 
+<?php
+  require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
+  $config = require __DIR__.'/../config/webapp.php';
+  $yii = new yii\web\Application($config);
+  $yii->run();
+ ?>
